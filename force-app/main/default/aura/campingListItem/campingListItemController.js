ffd804647367e736551795f5b95781da8a903205
Caleb <caleb@zennify.com>
2019-06-13 14:50:11 -0600

    Addition of pack item button

--- force-app/main/default/aura/campingListItem/campingListItemController.js
+++ force-app/main/default/aura/campingListItem/campingListItemController.js
@@ -1,5 +1,7 @@
 ({
-    myAction : function(component, event, helper) {
-
+    packItem : function(component, event, helper) {
+        var item = component.get('v.item');
+        item.Packed__c = true;
+        event.getSource().set('v.disabled', true);
     }
 })
