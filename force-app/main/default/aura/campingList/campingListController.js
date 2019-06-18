4527d8eb63c95ad3f4e3198736c9ee04be651f3a
Caleb <caleb@zennify.com>
2019-06-18 12:01:57 -0600

    addition of form and adding new item

--- force-app/main/default/aura/campingList/campingListController.js
+++ force-app/main/default/aura/campingList/campingListController.js
@@ -1,5 +1,18 @@
 ({
     myAction : function(component, event, helper) {
 
+    },
+    clickCreateItem : function(component, event, helper){
+        var validItem = component.find('itemform').reduce(function(validSoFar, inputCmp){
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        })
+        if (validItem){
+            var items = component.get('v.items');
+            items.push(component.get('v.newItem'));
+            component.set('v.items', items);
+            component.set('v.newItem', {'sobjectType' : 'Camping_Item__c',
+            'Name': '', 'Price__c': 0, 'Quantity__c': 0, 'Packed__c': false});
+        }
     }
 })
