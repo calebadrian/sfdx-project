01520b2ecf8e4a1bf5e304050d1d6599fcfedd31
Caleb <caleb@zennify.com>
2019-07-29 15:40:50 -0600

    New event and all changes for Step 5

--- force-app/main/default/aura/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -1,3 +1,9 @@
 ({
-
+    onBoatClick: function(component, event, helper){
+        var appEvent = component.getEvent('boatSelect');
+        appEvent.setParams({
+            boatId: component.get("v.boat").Id
+        })
+        appEvent.fire();
+    }
 })
