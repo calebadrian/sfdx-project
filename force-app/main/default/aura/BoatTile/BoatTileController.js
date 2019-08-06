dd91f9ee0f76679205f70d1acaa7df65eefb65fc
Caleb <caleb@zennify.com>
2019-08-05 21:44:57 -0600

    Completion of Aura Superbadge

--- force-app/main/default/aura/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -2,13 +2,22 @@
     onBoatClick: function(component, event, helper){
         var appEvent = component.getEvent('boatSelect');
         var appEvent1 = $A.get('e.c:BoatSelected');
+        var boat = component.get('v.boat');
         appEvent1.setParams({
-            boat: component.get('v.boat')
+            boat: boat
         })
         appEvent.setParams({
-            boatId: component.get("v.boat").Id
+            boatId: boat.Id
         })
         appEvent.fire();
         appEvent1.fire();
+        var appEvent2 = $A.get('e.c:PlotMapMarker');
+        appEvent2.setParams({
+            lat: boat.Geolocation__Latitude__s,
+            sObjectId: boat.Id,
+            long: boat.Geolocation__Longitude__s,
+            label: boat.Name
+        });
+        appEvent2.fire();
     }
 })
